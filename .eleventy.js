a4a14ff6c573ad650d25939cc416db7d16fbfecc
jucegt <julio2201@gmail.com>
2021-10-25 22:51:51 -0600

    Added AMP Plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+import amp from '@ampproject/eleventy-plugin-amp';
+
 export default (config) => {
+  config.addPlugin(amp, {
+    dir: {
+      output: '.11ty',
+    },
+  });
+
   return {
     dir: {
       input: 'src',

edc093728e78c4d458aa61c2b655f184a1852e34
jucegt <julio2201@gmail.com>
2021-11-01 22:18:20 -0600

    Added google font

--- src/layouts/base.11ty.js
+++ src/layouts/base.11ty.js
@@ -11,6 +11,9 @@ export const render = async (data) => {
       <meta name="generator" content="eleventy" />
       <meta name="viewport" content="width=device-width" />
       ${this.metadata(data)}
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet">
       <style amp-custom>
         ${await fs
           .readFile(path.resolve(__dirname, '../assets/css/style.css'))

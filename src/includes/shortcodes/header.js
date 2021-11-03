79b185b4a63aeefac143dd61bf4a76bea22d303f
jucegt <julio2201@gmail.com>
2021-11-03 00:57:31 -0600

    Changed some shortcodes

--- src/includes/shortcodes/header.js
+++ src/includes/shortcodes/header.js
@@ -1,11 +1,9 @@
 const header = (config) => {
-  config.addShortcode('header', () => {
+  config.addShortcode('header', (config) => {
     return /* html */ `
       <header class="header">
-        <a href="/" class="logo">
-          <svg xmlns="http://www.w3.org/2000/svg" width="130" height="35" viewBox="0 0 130 35">
-            <rect width="130" height="35" rx="17.5" opacity="0.05"/>
-          </svg>
+        <a href="/" class="logo" title="${config.title}">
+          <svg><use xlink:href="#logo" /></svg>
         </a>
       </header>
     `;
--- src/includes/shortcodes/index.js
+++ src/includes/shortcodes/index.js
@@ -1,10 +1,13 @@
 import metadata from './metadata';
 import header from './header';
+import footer from './footer';
+import svgs from './svgs';
 
 const shortcodes = (config) => {
   metadata(config);
   header(config);
-  return;
+  footer(config);
+  svgs(config);
 };
 
 export default shortcodes;

5d52ac217f4201c9f2d3360051e1e109d1932d28
jucegt <julio2201@gmail.com>
2021-11-05 00:11:07 -0600

    Autoprefix and minify the inlice css

--- src/includes/utilities/inline-css.js
+++ src/includes/utilities/inline-css.js
@@ -1,10 +1,20 @@
 import fs from 'fs/promises';
 import path from 'path';
 
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+
 const inlineCSS = async (file) => {
   const styles = await fs
     .readFile(path.resolve(__dirname, `../../assets/css/${file}`))
-    .then((css) => css);
+    .then(async (css) => {
+      const minified = await postcss([autoprefixer, cssnano]).process(css, {
+        from: '',
+        to: '',
+      });
+      return minified.css;
+    });
 
   return styles;
 };

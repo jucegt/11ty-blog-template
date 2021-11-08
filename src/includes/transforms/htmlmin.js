e6714c5a32d11b141975aca32bbbe45ca957a1ea
jucegt <julio2201@gmail.com>
2021-11-07 20:03:12 -0600

    Removed class minifier

--- src/includes/transforms/htmlmin.js
+++ src/includes/transforms/htmlmin.js
@@ -1,19 +1,14 @@
 import minifier from 'html-minifier';
-import posthtml from 'posthtml';
-import uglify from 'posthtml-minify-classnames';
 
 const htmlmin = (config) => {
   config.addTransform('htmlmin', async (content, outputPath) => {
     if (outputPath && outputPath.endsWith('.html')) {
-      const { html } = await posthtml()
-        .use(uglify({ filter: /^#svg-/ }))
-        .process(content);
-
-      const minified = minifier.minify(html, {
+      const minified = minifier.minify(content, {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true,
       });
+
       return minified;
     }
 

6796c9da47a35c5162bb39def81fe3ed22c9ae77
jucegt <julio2201@gmail.com>
2022-01-07 23:55:37 -0600

    Removed amp boilerplate files and added inline

--- src/layouts/base.11ty.js
+++ src/layouts/base.11ty.js
@@ -3,10 +3,11 @@ import inlineCSS from '../includes/utilities/inline-css';
 export const render = async (data) => {
   const { content, config, texts } = data;
   const social = this.social(config);
+  const textsByLang = texts[config.language];
 
   return /* html */ `
   <!doctype html>
-  <html ⚡ lang="${config.language}">
+  <html ⚡ lang="${config.language}" [class]="dark ? 'dark' : ''">
     <head>
       <!-- META -->
       <meta charset="utf-8" />
@@ -16,34 +17,23 @@ export const render = async (data) => {
       <!-- PRECONNECT -->
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <!-- PRELOAD -->
-      <link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js">
-      <link rel="preload" as="script" href="https://cdn.ampproject.org/v0/amp-bind-0.1.js" />
-      <!-- AMP -->
-      <script async src="https://cdn.ampproject.org/v0.js"></script>
-      <script async custom-element="amp-bind" src="https://cdn.ampproject.org/v0/amp-bind-0.1.js"></script>
+      ${this.amp()}
       <!-- FONTS -->
       <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet">
       <!-- STYLES -->
       <style amp-custom>
         ${await inlineCSS('style.css')}
       </style>
-      <style amp-boilerplate>
-        ${await inlineCSS('boilerplate/style.css')}
-      </style>
-      <noscript>
-        <style amp-boilerplate>
-          ${await inlineCSS('boilerplate/no-script.css')}
-        </style>
-      </noscript>
+      <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
       <!-- CANONICAL -->
       <link rel="canonical" href="." />
     </head>
     <body>
-      ${this.header(config)}
+      ${this.header(config, textsByLang)}
+      ${this.search()}
       ${content}
-      ${this.footer(social, config, texts[config.language])}
-      <div [class]="menu || search ? 'mask mask--show' : 'mask'" class="mask" on="tap:AMP.setState({menu: false, search: false})"></div>
+      ${this.footer(social, config, textsByLang)}
+      ${this.mask(textsByLang)}
       ${this.svgs()}
     </body>
   </html>

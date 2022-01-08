39e78103d5689b8d0912a072fabe56383ba4d069
jucegt <julio2201@gmail.com>
2022-01-07 23:58:23 -0600

    Added shortcodes

--- src/includes/shortcodes/index.js
+++ src/includes/shortcodes/index.js
@@ -1,14 +1,20 @@
 import metadata from './metadata';
+import amp from './amp';
 import header from './header';
+import search from './search';
 import footer from './footer';
 import social from './social';
+import mask from './mask';
 import svgs from './svgs';
 
 const shortcodes = (config) => {
   metadata(config);
+  amp(config);
   header(config);
+  search(config);
   footer(config);
   social(config);
+  mask(config);
   svgs(config);
 };
 
